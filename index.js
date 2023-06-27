916837f9f3b7ec8d9abe1cd222379d4ae6e4e0c3
Cameron Wilby <cameron@wilby.com>
2023-06-27 15:17:34 -0700

    Message update

--- index.js
+++ index.js
@@ -42,23 +42,17 @@ app.post('/send-sms', async (req, res) => {
 function buildContent(alert) {
     const description = Object.values(alert.annotations)[0];
     const value = JSON.stringify(alert.values);
-    const silenceUrl = alert.silenceURL;
     const observedAt = dayjs(alert.startsAt).format('YYYY-MM-DD HH:mm:ss Z');
     const notificationDeliveredAt = dayjs().format('YYYY-MM-DD HH:mm:ss Z');
     const observedSecondsAgo = dayjs(notificationDeliveredAt).diff(observedAt, 'seconds');
 
     let content = '';
 
+    content += alert.status === 'firing' ? 'FIRING\n\n' : 'RESOLVED\n\n';
     content += `Alert: ${description}\n\n`;
-
-    if (value) {
-        content += `Value: ${value}\n\n`;
-    } else {
-        content += `Value: No value\n\n`;
-    }
-
-    content += `Silence: ${silenceUrl}\n\n`;
-
+    content += value ? 'Value: ${value}\n\n' : 'No values\n\n';
+    content += `Panel: ${alert.panelURL}\n\n`;
+    content += `Silence: ${alert.silenceURL}\n`;
     content += `Observed ${observedSecondsAgo}s before this notification was delivered, at ${notificationDeliveredAt}`;
 
     return content;
