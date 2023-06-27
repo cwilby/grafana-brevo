b2b0d2239e216eb6ef28aecf6c77f2c5771c4086
Cameron Wilby <cameron@wilby.com>
2023-06-27 14:26:45 -0700

    Template update

--- index.js
+++ index.js
@@ -14,23 +14,22 @@ app.get('/', (req, res) => res.send('It\'s working!'));
 app.post('/send-sms', async (req, res) => {
     try {
         Brevo.ApiClient.instance.authentications['api-key'].apiKey = req.query.apiKey;
-        
-        const observedAt = new Date(Date.parse(req.body.alerts[0].startsAt)).toISOString();
-        const notificationDeliveredAt = new Date().toISOString();
-        const observedSecondsAgo = Math.round((new Date(notificationDeliveredAt) - new Date(observedAt)) / 1000);
 
-        const sendTransacSms = new Brevo.SendTransacSms();
-        sendTransacSms.sender = 'PinnacleSMS';
-        sendTransacSms.recipient = req.query.number;
-        sendTransacSms.content = `${req.body.message}\n\nObserved ${observedSecondsAgo}s before this notification was delivered, at ${notificationDeliveredAt}`;
-    
-        await new Brevo.TransactionalSMSApi().sendTransacSms(sendTransacSms);
+        const brevo = new Brevo.TransactionalSMSApi();
+
+        req.body.alerts.forEach(alert => {    
+            const sendTransacSms = new Brevo.SendTransacSms();
+            sendTransacSms.sender = 'PinnacleSMS';
+            sendTransacSms.recipient = req.query.number;
+            sendTransacSms.content = buildContent(alert);
+        
+            brevo.sendTransacSms(sendTransacSms);
+        });
+        
     
         res.status(201).send();
     } catch (e) {
-        res.status(500).json({
-            message: e.message
-        });
+        res.status(500).json({ message: e.message });
 
         const errorPath = path.resolve(__dirname, 'error.log');
 
@@ -39,6 +38,17 @@ app.post('/send-sms', async (req, res) => {
         fs.appendFileSync(errorPath, `${new Date().toISOString()} - ${e.message}\n`);
     }
 });
+
+function buildContent(alert) {
+    const description = Object.values(alert.annotations)[0];
+    const value = alert.values instanceof Array ? alert.values.join(', ') : alert.values;
+    const silenceUrl = alert.silenceURL;
+    const observedAt = new Date(Date.parse(req.body.alerts[0].startsAt)).toISOString();
+    const notificationDeliveredAt = new Date().toISOString();
+    const observedSecondsAgo = Math.round((new Date(notificationDeliveredAt) - new Date(observedAt)) / 1000);
+
+    return `Alert: ${description}\n\nValue: ${value}\n\nSilence: ${silenceUrl}\n\nObserved ${observedSecondsAgo}s before this notification was delivered, at ${notificationDeliveredAt}`;
+}
 
 const port = process.env.PORT || 32012;
 app.listen(port, () => {
