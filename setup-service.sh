8517ada6dbb35ade3ef31f39116936ee6ad1b7c2
Cameron Wilby <cameron@wilby.com>
2023-06-27 20:46:46 -0700

    sudo

--- setup-service.sh
+++ setup-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp /usr/local/share/grafana-brevo/grafana-brevo.service /etc/systemd/system/grafana-brevo.service
-systemctl start grafana-brevo
-systemctl enable grafana-brevo
+sudo cp /usr/local/share/grafana-brevo/grafana-brevo.service /etc/systemd/system/grafana-brevo.service
+sudo systemctl start grafana-brevo
+sudo systemctl enable grafana-brevo
